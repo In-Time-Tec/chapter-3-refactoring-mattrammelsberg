4aeb37b0a02fe9a4ed4e172346f166502a59e08e
Matt Rammelsberg <MatthewRammelsberg@truckstop.com>
2022-12-12 09:50:57 -0700

    2nd draft

--- TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -16,28 +16,14 @@ namespace TheatricalPlayersRefactoringKata
             foreach(var perf in invoice.Performances) 
             {
                 var play = plays[perf.PlayID];
-                var thisAmount = 0;
-                switch (play.Type) 
-                {
-                    case "tragedy":
-                        thisAmount = setAmountForTragedy(perf.Audience);
-                        break;
-                    case "comedy":
-                        thisAmount = setAmountForComedy(perf.Audience);
-                        volumeCredits += addExtraCreditsForEveryTenComedyAttendees(perf.Audience);
-                        break;
-                    default:
-                        throw new Exception("unknown type: " + play.Type);
-                }
-                // add volume credits
-                volumeCredits += Math.Max(perf.Audience - 30, 0);
+                var thisAmount = calculateAmountForPerf(perf, play.Type);
+                volumeCredits += calculateVolCreditsForPerf(perf.Audience, play.Type);
 
                 // print line for this order
                 result += String.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
                 totalAmount += thisAmount;
             }
-            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
-            result += String.Format("You earned {0} credits\n", volumeCredits);
+            result += String.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100)) + String.Format("You earned {0} credits\n", volumeCredits);
             return result;
         }
 
@@ -59,6 +45,30 @@ namespace TheatricalPlayersRefactoringKata
 
         public int addExtraCreditsForEveryTenComedyAttendees(int perfAudience) {
             return (int)Math.Floor((decimal)perfAudience / 5);
+        }
+
+        public int calculateAmountForPerf(Performance perf, String playType) {
+            var thisAmount = 0;
+            switch (playType) 
+            {
+                case "tragedy":
+                    thisAmount = setAmountForTragedy(perf.Audience);
+                    break;
+                case "comedy":
+                    thisAmount = setAmountForComedy(perf.Audience);
+                    break;
+                default:
+                    throw new Exception("unknown type: " + playType);
+            }
+            return thisAmount;
+        }
+
+        public int calculateVolCreditsForPerf(int audience, String playType) {
+            var credits = Math.Max(audience - 30, 0);
+            if(playType == "comedy") {
+                credits+= addExtraCreditsForEveryTenComedyAttendees(audience);
+            }
+            return credits;
         }
     }
 }
